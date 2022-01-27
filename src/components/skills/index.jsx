44a477a4a4b5e20da3cf48ce78a9afb8ab41a464
rianmfir <rianmfirdaus@gmail.com>
2022-01-27 14:19:04 +0700

    Solving

--- src/components/skills/index.jsx
+++ src/components/skills/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { CSS, HTML, Java, Javascript, MySQL, NodeJS, ReactJS } from "../../assets";
-import '../index.css'
+import '../../index.css'
 
 const Skills = () => {
 
